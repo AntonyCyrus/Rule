7d89da7c42820f50189981b503f7d38af552aa34
AntonyCyrus <99586274+AntonyCyrus@users.noreply.github.com>
2023-04-07 16:33:27 +0800

    Create Surge音乐媒体.py

---
+++ action/Surge音乐媒体.py
@@ -0,0 +1,33 @@
+import requests
+import pytz
+from datetime import datetime
+
+result = []
+
+# 定义需要请求的链接
+urls = [
+    "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Deezer/Deezer.list",
+    "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Qobuz/Qobuz.list",
+    "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Spotify/Spotify.list",
+    "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/TIDAL/TIDAL.list"
+]
+
+# 遍历链接列表，依次请求链接内容并将结果保存到 result 列表中
+for url in urls:
+    try:
+        raw = requests.get(url).text
+        result.extend([item for item in raw.split("\n") if item.strip() and not item.startswith('#')])
+    except requests.exceptions.RequestException as e:
+        print("Error getting data:", e)
+
+# 获取当前时间并格式化
+tz = pytz.timezone('Asia/Shanghai')
+now = datetime.now(tz)
+time_str = now.strftime("%Y-%m-%d %H:%M:%S")
+
+# 拼接要写入文件的字符串
+result_text = "# Music.list\n# Generated at " + time_str + "\n\n" + "\n".join(result) + "\n\n# This configuration file has been generated successfully."
+
+# 将字符串写入文件
+with open("./Surge/Music.list", "w", encoding="utf-8") as f:
+    f.write(result_text)
