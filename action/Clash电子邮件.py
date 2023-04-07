ab3553baf96d011e97b7ed035629c3ccf31bebe3
AntonyCyrus <99586274+AntonyCyrus@users.noreply.github.com>
2023-04-07 14:39:10 +0800

    Create Clash电子邮件.py

---
+++ action/Clash电子邮件.py
@@ -0,0 +1,27 @@
+import requests
+
+rawMail = ""
+rawAppleMail = ""
+rawProtonmail = ""
+
+print("Fetching remote resources")
+
+try:
+    rawMail = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Mail/Mail.yaml").text
+    rawAppleMail = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/AppleMail/AppleMail.yaml").text
+    rawProtonmail = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Protonmail/Protonmail.yaml").text
+except requests.exceptions.RequestException as e:
+    print("Error occurred when requesting remote resources:", e)
+
+print("Processing fetched resources and writing to file")
+
+result = ['payload:']
+for rawresult in [rawMail, rawAppleMail, rawProtonmail]:
+    result.extend([item.rstrip() for item in rawresult.split('\n') if not (item.startswith('#') or item.startswith('payload:'))])
+result_text = '\n'.join(result)
+
+try:
+    with open("./Clash/Mail.yaml", "w") as f:
+        f.write(result_text)
+except IOError as e:
+    print("Error occurred when writing to file:", e)
