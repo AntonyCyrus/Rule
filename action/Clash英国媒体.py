0a75ccb6377e86b047a29b9e74aec86e8803a722
AntonyCyrus <99586274+AntonyCyrus@users.noreply.github.com>
2023-04-07 14:41:51 +0800

    Create Clash英国媒体.py

---
+++ action/Clash英国媒体.py
@@ -0,0 +1,26 @@
+import requests
+
+rawAll4 = ""
+rawBBC = ""
+rawBritboxUK = ""
+rawDailymail = ""
+rawMy5 = ""
+try:
+    rawAll4 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/All4/All4.yaml").text
+    rawBBC = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/BBC/BBC.yaml").text
+    rawBritboxUK = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/BritboxUK/BritboxUK.yaml").text
+    rawDailymail = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Dailymail/Dailymail.yaml").text
+    rawMy5 = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/My5/My5.yaml").text
+except requests.exceptions.RequestException as e:
+    print("Error occurred when requesting remote resources:", e)
+
+result = ['payload:']
+for rawresult in [rawAll4, rawBBC, rawBritboxUK, rawDailymail, rawMy5]:
+    result.extend([item.rstrip() for item in rawresult.split('\n') if not (item.startswith('#') or item.startswith('payload:'))])
+result_text = '\n'.join(result)
+
+try:
+    with open("./Clash/GBMedia.yaml", "w") as f:
+        f.write(result_text)
+except IOError as e:
+    print("Error occurred when writing to file:", e)
