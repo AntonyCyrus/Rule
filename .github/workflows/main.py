62e05b8b5cb7f3cbb5398572514e783d9c5efd77
AntonyCyrus <99586274+AntonyCyrus@users.noreply.github.com>
2023-04-07 08:07:53 +0800

    Create main.py

---
+++ .github/workflows/main.py
@@ -0,0 +1,47 @@
+# This is a basic workflow to help you get started with Actions
+
+name: CI
+
+on:
+  push:
+    branches:
+      - main
+  schedule:
+    - cron: '0 20 * * *'
+    
+# Controls when the workflow will run
+  # Triggers the workflow on push or pull request events but only for the "main" branch
+  pull_request:
+    branches: [ "main" ]
+
+  # Allows you to run this workflow manually from the Actions tab
+  workflow_dispatch:
+
+# A workflow run is made up of one or more jobs that can run sequentially or in parallel
+jobs:
+  # This workflow contains a single job called "build"
+  build:
+    # The type of runner that the job will run on
+    runs-on: ubuntu-latest
+
+    # Steps represent a sequence of tasks that will be executed as part of the job
+    steps:
+      # Checks-out your repository under $GITHUB_WORKSPACE, so your job can access it
+      - uses: actions/checkout@v3
+
+      # Runs a single command using the runners shell
+      - name: Run a one-line script
+        run: for f in action/*.py; do python "$f"; done
+
+      - name: Commit files
+        run: |
+          git config --local user.email "z4xxby5k@duck.com"
+          git config --local user.name "AntonyCyrus"
+          git add ./*
+          git commit -m "Add changes" -a
+      - name: Push changes
+        uses: ad-m/github-push-action@master
+        with:
+          github_token: ${{ secrets.ACTIONS_TOKEN }}
+          branch: ${{ github.ref }}
+.github/workflows
