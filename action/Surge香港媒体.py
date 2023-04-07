a5339e6e6fd4e2d145fad57a9ab97ff55bc618d5
AntonyCyrus <99586274+AntonyCyrus@users.noreply.github.com>
2023-04-07 15:18:11 +0800

    Update Surge香港媒体.py

--- action/Surge香港媒体.py
+++ action/Surge香港媒体.py
@@ -4,47 +4,32 @@ from datetime import datetime
 
 result = []
 
-try:
-    rawJOOX = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/JOOX/JOOX.list").text
-    result.extend([item for item in rawJOOX.split("\n") if item.strip() and not item.startswith('#')])
-except requests.exceptions.RequestException as e:
-    print("Error getting JOOX data:", e)
+# 定义需要请求的链接
+urls = [
+    "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/JOOX/JOOX.list",
+    "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/MOOV/MOOV.list",
+    "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/myTVSUPER/myTVSUPER.list",
+    "https://raw.githubusercontent.com/AntonyCyrus/Rule/main/Surge/NowE.list",
+    "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/PCCW/PCCW.list",
+    "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/TVB/TVB.list"
+]
 
-try:
-    rawMOOV = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/MOOV/MOOV.list").text
-    result.extend([item for item in rawMOOV.split("\n") if item.strip() and not item.startswith('#')])
-except requests.exceptions.RequestException as e:
-    print("Error getting MOOV data:", e)
+# 遍历链接列表，依次请求链接内容并将结果保存到 result 列表中
+for url in urls:
+    try:
+        raw = requests.get(url).text
+        result.extend([item for item in raw.split("\n") if item.strip() and not item.startswith('#')])
+    except requests.exceptions.RequestException as e:
+        print("Error getting data:", e)
 
-try:
-    rawmyTVSUPER = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/myTVSUPER/myTVSUPER.list").text
-    result.extend([item for item in rawmyTVSUPER.split("\n") if item.strip() and not item.startswith('#')])
-except requests.exceptions.RequestException as e:
-    print("Error getting myTVSUPER data:", e)
-
-try:
-    rawNowE = requests.get("https://raw.githubusercontent.com/AntonyCyrus/Rule/main/Surge/NowE.list").text
-    result.extend([item for item in rawNowE.split("\n") if item.strip() and not item.startswith('#')])
-except requests.exceptions.RequestException as e:
-    print("Error getting NowE data:", e)
-
-try:
-    rawPCCW = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/PCCW/PCCW.list").text
-    result.extend([item for item in rawPCCW.split("\n") if item.strip() and not item.startswith('#')])
-except requests.exceptions.RequestException as e:
-    print("Error getting PCCW data:", e)
-
-try:
-    rawTVB = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/TVB/TVB.list").text
-    result.extend([item for item in rawTVB.split("\n") if item.strip() and not item.startswith('#')])
-except requests.exceptions.RequestException as e:
-    print("Error getting TVB data:", e)
-
+# 获取当前时间并格式化
 tz = pytz.timezone('Asia/Shanghai')
 now = datetime.now(tz)
 time_str = now.strftime("%Y-%m-%d %H:%M:%S")
 
+# 拼接要写入文件的字符串
 result_text = "# HKMedia.list\n# Generated at " + time_str + "\n\n" + "\n".join(result) + "\n\n# This configuration file has been generated successfully."
 
+# 将字符串写入文件
 with open("./Surge/HKMedia.list", "w", encoding="utf-8") as f:
     f.write(result_text)
