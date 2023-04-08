bd5f958c6a48cd30068e861da2d18a4902c23b9c
AntonyCyrus <99586274+AntonyCyrus@users.noreply.github.com>
2023-04-08 12:05:12 +0800

    Create Clash搜索引擎.py

---
+++ action/Clash搜索引擎.py
@@ -0,0 +1,20 @@
+import requests
+
+rawDuckduckgo = ""
+rawYandex = ""
+try:
+    rawDuckduckgo = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Duckduckgo/Duckduckgo.yaml").text
+    rawYandex = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Yandex/Yandex.yaml").text
+except requests.exceptions.RequestException as e:
+    print("Error occurred when requesting remote resources:", e)
+
+result = ['payload:']
+for rawresult in [rawDuckduckgo, rawYandex]:
+    result.extend([item.rstrip() for item in rawresult.split('\n') if not (item.startswith('#') or item.startswith('payload:'))])
+result_text = '\n'.join(result)
+
+try:
+    with open("./Clash/SearchEngine.yaml", "w") as f:
+        f.write("\n".join(result))
+except IOError as e:
+    print("Error occurred when writing to file:", e)
