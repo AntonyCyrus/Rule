a685785c41d1f5f93af1f3eed32825a81918dd23
AntonyCyrus <99586274+AntonyCyrus@users.noreply.github.com>
2023-04-18 16:41:10 +0800

    Create Quantumult X 日本媒体.py

---
+++ action/Quantumult X 日本媒体.py
@@ -0,0 +1,40 @@
+import requests
+import pytz
+from datetime import datetime
+
+result = []
+
+# 定义需要请求的链接
+urls = [
+    "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/QuantumultX/AbemaTV/AbemaTV.list",
+    "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/QuantumultX/DMM/DMM.list",
+    "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/QuantumultX/HuluJP/HuluJP.list",
+    "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/QuantumultX/Niconico/Niconico.list",
+    "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/QuantumultX/Nikkei/Nikkei.list",
+    "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/QuantumultX/Nintendo/Nintendo.list",
+    "https://raw.githubusercontent.com/HotKids/Rules/master/Quantumult/X/Filter/Paravi.list",
+    "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/QuantumultX/Pixiv/Pixiv.list",
+    "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/QuantumultX/Pixnet/Pixnet.list",
+    "https://raw.githubusercontent.com/HotKids/Rules/master/Quantumult/X/Filter/TVer.list",
+    "https://raw.githubusercontent.com/HotKids/Rules/master/Quantumult/X/Filter/U-NEXT.list"
+]
+
+# 遍历链接列表，依次请求链接内容并将结果保存到 result 列表中
+for url in urls:
+    try:
+        raw = requests.get(url).text
+        result.extend([item for item in raw.split("\n") if item.strip() and not item.startswith('#')])
+    except requests.exceptions.RequestException as e:
+        print("Error getting data:", e)
+
+# 获取当前时间并格式化
+tz = pytz.timezone('Asia/Shanghai')
+now = datetime.now(tz)
+time_str = now.strftime("%Y-%m-%d %H:%M:%S")
+
+# 拼接要写入文件的字符串
+result_text = "# JPMedia.list\n# Generated at " + time_str + "\n\n" + "\n".join(result) + "\n\n# This configuration file has been generated successfully."
+
+# 将字符串写入文件
+with open("./Quantumult X/JPMedia.list", "w", encoding="utf-8") as f:
+    f.write(result_text)
