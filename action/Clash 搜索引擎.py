e7bd7e92ef938240b1d7d764e4fb762198347124
AntonyCyrus <99586274+AntonyCyrus@users.noreply.github.com>
2023-04-28 05:32:53 +0800

    Update Clash 搜索引擎.py

--- action/Clash 搜索引擎.py
+++ action/Clash 搜索引擎.py
@@ -5,12 +5,12 @@ rawYandex = ""
 try:
     rawDuckduckgo = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Duckduckgo/Duckduckgo.yaml").text
     rawYandex = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Yandex/Yandex.yaml").text
-    rawAol+Ask+Brave+Ecosia+Qwant+Startpage+webcrawler+wolframalpha+Yahoo! = requests.get("https://raw.githubusercontent.com/AntonyCyrus/Rule/main/Clash/Aol%2BAsk%2BBrave%2BEcosia%2BQwant%2BStartpage%2Bwebcrawler%2Bwolframalpha%2BYahoo!.yaml").text
+    rawAol+Ask+Brave+Ecosia+Qwant+Startpage+webcrawler+wolframalpha+Yahoo = requests.get("https://raw.githubusercontent.com/AntonyCyrus/Rule/main/Clash/Aol%2BAsk%2BBrave%2BEcosia%2BQwant%2BStartpage%2Bwebcrawler%2Bwolframalpha%2BYahoo.yaml").text
 except requests.exceptions.RequestException as e:
     print("Error occurred when requesting remote resources:", e)
 
 result = ['payload:']
-for rawresult in [rawDuckduckgo, rawYandex,rawAol+Ask+Brave+Ecosia+Qwant+Startpage+webcrawler+wolframalpha+Yahoo!]:
+for rawresult in [rawDuckduckgo, rawYandex,rawAol+Ask+Brave+Ecosia+Qwant+Startpage+webcrawler+wolframalpha+Yahoo]:
     result.extend([item.rstrip() for item in rawresult.split('\n') if not (item.startswith('#') or item.startswith('payload:'))])
 result_text = '\n'.join(result)
 
