0dc1c9728b09cdf4fefcddd2c2c42e939ee1d677
AntonyCyrus <99586274+AntonyCyrus@users.noreply.github.com>
2023-04-28 09:36:40 +0800

    Create Shadowrocket 台湾媒体.py

---
+++ action/Shadowrocket 台湾媒体.py
@@ -0,0 +1,41 @@
+import requests
+import pytz
+from datetime import datetime
+
+result = []
+
+# 定义需要请求的链接
+urls = [
+    "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Shadowrocket/Bahamut/Bahamut.list",
+    "https://raw.githubusercontent.com/HotKids/Rules/master/Surge/RULE-SET/CATCHPLAY%2B.list",
+    "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Shadowrocket/friDay/friDay.list",
+    "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Shadowrocket/HBOAsia/HBOAsia.list",
+    "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Shadowrocket/HamiVideo/HamiVideo.list",
+    "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Shadowrocket/KKBOX/KKBOX.list",
+    "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Shadowrocket/KKTV/KKTV.list",
+    "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Shadowrocket/LiTV/LiTV.list",
+    "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Shadowrocket/LineTV/LineTV.list",
+    "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Shadowrocket/MOMOShop/MOMOShop.list",
+    "https://raw.githubusercontent.com/HotKids/Rules/master/Surge/RULE-SET/myVideo.list",
+    "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Shadowrocket/TaiWanGood/TaiWanGood.list"
+]
+
+# 遍历链接列表，依次请求链接内容并将结果保存到 result 列表中
+for url in urls:
+    try:
+        raw = requests.get(url).text
+        result.extend([item for item in raw.split("\n") if item.strip() and not item.startswith('#')])
+    except requests.exceptions.RequestException as e:
+        print("Error getting data:", e)
+
+# 获取当前时间并格式化
+tz = pytz.timezone('Asia/Shanghai')
+now = datetime.now(tz)
+time_str = now.strftime("%Y-%m-%d %H:%M:%S")
+
+# 拼接要写入文件的字符串
+result_text = "# TWMedia.list\n# Generated at " + time_str + "\n\n" + "\n".join(result) + "\n\n# This configuration file has been generated successfully."
+
+# 将字符串写入文件
+with open("./Shadowrocket/TWMedia.list", "w", encoding="utf-8") as f:
+    f.write(result_text)
