ce232d4bb41d2788d77a0bc243b08c93df2489ff
AntonyCyrus <99586274+AntonyCyrus@users.noreply.github.com>
2023-05-10 00:43:18 +0800

    Update Clash 香港媒体.py

--- action/Clash 香港媒体.py
+++ action/Clash 香港媒体.py
@@ -1,30 +1,32 @@
 import requests
 
-rawJOOX = ""
-rawMOOV = ""
-rawmyTVSUPER = ""
-rawNowE = ""
-rawPCCW = ""
-rawTVB = ""
-rawViuTV = ""
-try:
-    rawJOOX = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/JOOX/JOOX.yaml").text
-    rawMOOV = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/MOOV/MOOV.yaml").text
-    rawmyTVSUPER = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/myTVSUPER/myTVSUPER.yaml").text
-    rawNowE = requests.get("https://raw.githubusercontent.com/AntonyCyrus/Rule/main/Clash/NowE.yaml").text
-    rawPCCW = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/PCCW/PCCW.yaml").text
-    rawTVB = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/TVB/TVB.yaml").text
-    rawViuTV = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/ViuTV/ViuTV.yaml").text
-except requests.exceptions.RequestException as e:
-    print("Error occurred when requesting remote resources:", e)
+urls = [
+    "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/JOOX/JOOX.yaml",
+    "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/MOOV/MOOV.yaml",
+    "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/myTVSUPER/myTVSUPER.yaml",
+    "https://raw.githubusercontent.com/AntonyCyrus/Rule/main/Clash/NowE.yaml",
+    "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/PCCW/PCCW.yaml",
+    "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/TVB/TVB.yaml",
+    "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/ViuTV/ViuTV.yaml"
+]
 
-result = ['payload:']
-for rawresult in [rawJOOX, rawMOOV, rawmyTVSUPER, rawNowE, rawPCCW, rawTVB, rawViuTV]:
-    result.extend([item.rstrip() for item in rawresult.split('\n') if not (item.startswith('#') or item.startswith('payload:'))])
-result_text = '\n'.join(result)
+results = set()
+
+for url in urls:
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        raw_result = response.text
+        # 去掉注释和 payload 标记，只保留规则内容
+        result_lines = [line.strip() for line in raw_result.split('\n') if line and not line.startswith(('#', 'payload:'))]
+        results.update(result_lines)
+    except requests.exceptions.RequestException as e:
+        print(f"Error occurred when requesting {url}: {e}")
+
+result_text = '\n'.join(['payload:'] + sorted(results))
 
 try:
     with open("./Clash/HKMedia.yaml", "w") as f:
-        f.write("\n".join(result))
+        f.write(result_text)
 except IOError as e:
     print("Error occurred when writing to file:", e)
