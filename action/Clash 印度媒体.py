7a26bcee3522da2241f21b9abdc12f301b2aa110
AntonyCyrus <99586274+AntonyCyrus@users.noreply.github.com>
2023-05-10 03:39:47 +0800

    Update Clash 印度媒体.py

--- action/Clash 印度媒体.py
+++ action/Clash 印度媒体.py
@@ -1,19 +1,24 @@
 import requests
 
-rawHotstar = ""
 rawZeeTV = ""
+
 try:
     rawZeeTV = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/ZeeTV/ZeeTV.yaml").text
 except requests.exceptions.RequestException as e:
     print("Error occurred when requesting remote resources:", e)
 
 result = ['payload:']
+unique_lines = set()
 for rawresult in [rawZeeTV]:
-    result.extend([item.rstrip() for item in rawresult.split('\n') if not (item.startswith('#') or item.startswith('payload:'))])
+    for item in rawresult.split('\n'):
+        if item.startswith('#') or item.startswith('payload:') or item in unique_lines:
+            continue
+        result.append(item.rstrip())
+        unique_lines.add(item)
 result_text = '\n'.join(result)
 
 try:
     with open("./Clash/INMedia.yaml", "w") as f:
-        f.write("\n".join(result))
+        f.write(result_text)
 except IOError as e:
     print("Error occurred when writing to file:", e)
