1575f9612817a800a02be69b83bd740e927e5912
AntonyCyrus <99586274+AntonyCyrus@users.noreply.github.com>
2023-06-14 12:14:25 +0800

    Update Clash 日本媒体.py

--- action/Clash 日本媒体.py
+++ action/Clash 日本媒体.py
@@ -19,7 +19,6 @@ try:
     rawHuluJP = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/HuluJP/HuluJP.yaml").text
     rawNiconico = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Niconico/Niconico.yaml").text
     rawNikkei = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Nikkei/Nikkei.yaml").text
-    rawNintendo = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Nintendo/Nintendo.yaml").text
     rawParavi = requests.get("https://raw.githubusercontent.com/HotKids/Rules/master/Clash/RuleSet/Paravi.yaml").text
     rawPixiv = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Pixiv/Pixiv.yaml").text
     rawPixnet = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Pixnet/Pixnet.yaml").text
@@ -31,7 +30,7 @@ except requests.exceptions.RequestException as e:
 # 处理变量数据
 result = ['payload:']
 unique_lines = set()
-for rawresult in [rawAbemaTV, rawDMM, rawHuluJP, rawNiconico, rawNikkei, rawNintendo, rawParavi, rawPixiv, rawPixnet, rawTver, rawU_NEXT]:
+for rawresult in [rawAbemaTV, rawDMM, rawHuluJP, rawNiconico, rawNikkei, rawParavi, rawPixiv, rawPixnet, rawTver, rawU_NEXT]:
     for item in rawresult.split('\n'):
         if item.startswith('#') or item.startswith('payload:') or item in unique_lines:
             continue
