fa6c4c1773f3559401259200c9dcb19d0faef00d
AntonyCyrus <99586274+AntonyCyrus@users.noreply.github.com>
2023-06-17 09:08:03 +0800

    Update Loon 美国媒体.py

--- action/Loon 美国媒体.py
+++ action/Loon 美国媒体.py
@@ -6,6 +6,7 @@ result = []
 
 # 定义需要请求的链接
 urls = [
+    "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/GoogleVoice/GoogleVoice.list",
     "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/9News/9News.list",
     "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/Americasvoice/Americasvoice.list",
     "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/Bestbuy/Bestbuy.list",
