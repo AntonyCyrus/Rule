544ff32ffaa76683097a4702a45fa2ce12f01a63
AntonyCyrus <99586274+AntonyCyrus@users.noreply.github.com>
2023-10-21 08:05:14 +0000

    Update Clash 印度媒体.yaml.py

--- action/Clash 印度媒体.yaml.py
+++ action/Clash 印度媒体.yaml.py
@@ -8,13 +8,18 @@ except requests.exceptions.RequestException as e:
     print("Error occurred when requesting remote resources:", e)
 
 result = ['payload:']
-unique_lines = set()
+unique_lines = set()  # 存储唯一行的集合
+seen_lines = set()    # 存储已经看到的行的集合
+
 for rawresult in [rawZeeTV]:
     for item in rawresult.split('\n'):
-        if item.startswith('#') or item.startswith('payload:') or item in unique_lines:
+        item = item.rstrip()  # 去除尾部的空白字符
+        if item.startswith('#') or item.startswith('payload:') or item in seen_lines:
             continue
-        result.append(item.rstrip())
+        result.append(item)
+        seen_lines.add(item)
         unique_lines.add(item)
+
 result_text = '\n'.join(result)
 
 try:
