26aec264d987a6e919ec9e19efc1892757a52342
AntonyCyrus <99586274+AntonyCyrus@users.noreply.github.com>
2023-10-21 07:53:35 +0000

    Update Clash 电子邮件.yaml.py

--- action/Clash 电子邮件.yaml.py
+++ action/Clash 电子邮件.yaml.py
@@ -14,13 +14,25 @@ except requests.exceptions.RequestException as e:
 print("Processing fetched resources and writing to file")
 
 result = ['payload:']
-unique_lines = set()
+unique_lines = set()  # 存储唯一行的集合
+seen_lines = set()    # 存储已经看到的行的集合
+has_domain_suffix_mail_me = False
+
 for rawresult in [rawMail, rawProtonmail]:
     for item in rawresult.split('\n'):
-        if item.startswith('#') or item.startswith('payload:') or item in unique_lines:
+        item = item.rstrip()  # 去除尾部的空白字符
+        if item.startswith('#') or item.startswith('payload:') or item in seen_lines:
             continue
-        result.append(item.rstrip())
+        if item == '- DOMAIN-SUFFIX,mail.me.com':
+            has_domain_suffix_mail_me = True
+        result.append(item)
+        seen_lines.add(item)
         unique_lines.add(item)
+
+# 如果文件中没有 `- DOMAIN-SUFFIX,mail.me.com`，则添加这一行
+if not has_domain_suffix_mail_me:
+    result.append('- DOMAIN-SUFFIX,mail.me.com')
+
 result_text = '\n'.join(result)
 
 try:
