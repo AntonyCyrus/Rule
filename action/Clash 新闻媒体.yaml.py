f876c7b3b03e78c5e5810b04cb6fb07d072d2fa1
AntonyCyrus <99586274+AntonyCyrus@users.noreply.github.com>
2023-10-28 19:44:38 +0000

    Update Clash 新闻媒体.yaml.py

--- action/Clash 新闻媒体.yaml.py
+++ action/Clash 新闻媒体.yaml.py
@@ -8,6 +8,7 @@ rawInitium_Media = ""
 rawNYPost = ""
 rawNYTimes = ""
 rawVOA = ""
+rawWSJ = ""
 
 print("Fetching remote resources")
 
@@ -20,6 +21,7 @@ try:
     rawNYPost = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/NYPost/NYPost.yaml").text
     rawNYTimes = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/NYTimes/NYTimes.yaml").text
     rawVOA = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/VOA/VOA.yaml").text
+    rawWSJ = requests.get("https://raw.githubusercontent.com/AntonyCyrus/Rule/main/Clash/WSJ.yaml").text
 except requests.exceptions.RequestException as e:
     print("Error occurred when requesting remote resources:", e)
 
@@ -27,7 +29,7 @@ print("Processing fetched resources and writing to file")
 
 result = ['payload:']
 unique_lines = set()
-for rawresult in [rawAFP, rawBloomberg, rawFlipBoard, rawHuffpost, rawInitium_Media, rawNYPost, rawNYTimes, rawVOA]:
+for rawresult in [rawAFP, rawBloomberg, rawFlipBoard, rawHuffpost, rawInitium_Media, rawNYPost, rawNYTimes, rawVOA, rawWSJ]:
     for item in rawresult.split('\n'):
         if item.startswith('#') or item.startswith('payload:') or item in unique_lines:
             continue
