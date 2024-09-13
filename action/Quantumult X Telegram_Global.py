5257932d83d9600050f0c53da951fc12feb6b9d9
AntonyCyrus <99586274+AntonyCyrus@users.noreply.github.com>
2024-09-13 06:59:45 +0000

    Update Quantumult X Telegram_Global.py

--- action/Quantumult X Telegram_Global.py
+++ action/Quantumult X Telegram_Global.py
@@ -7,9 +7,9 @@ result = []
 # 定义需要请求的链接
 telegram_url = "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/QuantumultX/Telegram/Telegram.list"
 region_urls = [
-    "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Quantumult X/TelegramUS/TelegramUS.list",
-    "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Quantumult X/TelegramSG/TelegramSG.list",
-    "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Quantumult X/TelegramNL/TelegramNL.list"
+    "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/QuantumultX/TelegramUS/TelegramUS.list",
+    "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/QuantumultX/TelegramSG/TelegramSG.list",
+    "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/QuantumultX/TelegramNL/TelegramNL.list"
 ]
 
 # 获取 Telegram Global 的规则文件内容
@@ -45,7 +45,7 @@ result_text = "# Telegram_Global.list\n# Generated at " + time_str + "\n\n" + "\
 
 # 将字符串写入文件
 try:
-    with open("./Quantumult X/Telegram_Global.list", "w", encoding="utf-8") as f:
+    with open("./QuantumultX/Telegram_Global.list", "w", encoding="utf-8") as f:
         f.write(result_text)
 except IOError as e:
     print("Error occurred when writing to file:", e)
