0d724e6ad0bc53a16591aeacf7d59cb63c5b7083
AntonyCyrus <99586274+AntonyCyrus@users.noreply.github.com>
2026-05-15 20:47:48 +0000

    Update Surge 印度媒体.py

--- action/Surge 印度媒体.py
+++ action/Surge 印度媒体.py
@@ -86,7 +86,7 @@ def fetch_list(url):
         # ====================================
 
         headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/605.1.15 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/605.1.15 Edg/120.0.0.0"
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0"
         }
 
 
